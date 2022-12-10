2bdc72d0321481a0b6823018bf3d87015cfbd3eb
Memmedov <sherif_memmedov@winadpro.com>
2022-12-10 21:42:07 +0400

    change1

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ let addTodo = document.querySelector(".list"); //ad to do
 let listGroup = document.querySelector(".list-group"); //ul
 
 addTodo.addEventListener("click", () => {
+  
   if (input.value.trim() != "") {
     listGroup.innerHTML += `<li class="list-group-items"> <span class="todo-text">${input.value}</span>
     <div class="delete-todo"><img src="/icons/Group 77.svg" alt=""><img src="/icons/Group 70.svg"
             alt=""></div>
 </li>`;
+
     input.value = "";
   } else {
     alert("to do elave edin");
@@ -20,5 +22,9 @@ document.body.addEventListener("click", (e) => {
 if(e.target.classList.contains("delete-todo")){
   e.target.parentElement.remove()
 }
+
+});
+input.addEventListener("click", () => {
+  input.placeholder.onfocus = none;
 
 });
